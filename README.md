3f9086c6e1dccb5622784ccaa43cba6179bd6542
sshnuke333 <sshnuke@protonmail.com>
2022-01-04 23:33:09 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Markdown Previewer
+
+Part of FreeCodeCamp's Front End Development Libraries Project challenge built
+using React and styled-components
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <>
+            <textarea id="editor"></textarea>
+            <p id="preview"></p>
+        </>
+    );
+}
